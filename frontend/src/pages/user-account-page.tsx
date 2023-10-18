055ef66690f029adfb549298b4235ac702299e9f
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2023-10-18 01:38:44 -0300

    feat: create a basic user-account-page

--- frontend/src/pages/user-account-page.tsx
+++ frontend/src/pages/user-account-page.tsx
@@ -1,6 +1,69 @@
+import { useGetUserAccountQuery } from "@/store/api";
+import { selectUserAccount } from "@/store/user-account-slice";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 function UserAccountPage() {
+  const { isLoading } = useGetUserAccountQuery()
+  const userAccount = useSelector(selectUserAccount)
+
+
   return (
-    <h1>User Account Page</h1>
+    <>
+      {isLoading && <p>Loading...</p>}
+      {userAccount && (
+        <table>
+          <tbody>
+            <tr>
+              <td>email</td>
+              <td>{userAccount.email}</td>
+            </tr>
+            <tr>
+              <td>Profiles</td>
+              <td>{userAccount.numProfiles()}</td>
+            </tr>
+            <tr>
+              <td>Payment Methods</td>
+              <td>{userAccount.numPaymentMethods()}</td>
+            </tr>
+            <tr>
+              <td>Persons</td>
+              <td>{userAccount.numPersons()}</td>
+            </tr>
+            <tr>
+              <td>Addresses</td>
+              <td>{userAccount.numAddresses()}</td>
+            </tr>
+            <tr>
+              <td>Meters</td>
+              <td>{userAccount.numMeters()}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+      {userAccount && userAccount.profiles.length > 0 && (
+        <table>
+          <tbody>
+            <tr>
+              <td>Profiles</td>
+              <td />
+            </tr>
+            {userAccount.profiles.map((profile) => (
+              <tr key={profile.id}>
+                <td>
+                  <Link to={`profile/${profile.id}`}>
+                    {profile.name}
+                  </Link>
+                </td>
+                <td>
+                  <button>remove</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 }
 
